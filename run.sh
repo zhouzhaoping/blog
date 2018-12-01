2b06e7d32a3ae129932bf493926d2c4722b75b9f
zhouzhaoping <zhouzhaoping11@gmail.com>
2018-12-02 01:12:42 +0800

    fix init error

--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@
 
 work_path=$(dirname $(readlink -f $0))
 
+# gen
+docker run -it \
+    -v $work_path/hexo:/Hexo \
+    hexo-docker \
+    hexo g
+
 # 预览
 docker run -it -d \
     -p 4000:4000 \
--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-work_path=$(dirname $(readlink -f $0))
-
-# 本地测试
-docker run -it \
-    -v $work_path/hexo:/Hexo \
-    hexo-docker \
-    hexo g
