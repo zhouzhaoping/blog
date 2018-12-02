ec2a6b9764b4dfe2dae3df5debc70807beaff20b
zhouzhaoping <zhouzhaoping11@gmail.com>
2018-12-03 02:33:43 +0800

    debug.sh

---
+++ debug.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+work_path=$(dirname $(readlink -f $0))
+
+# gen
+docker run -it \
+    -v $work_path/hexo:/Hexo \
+    hexo-docker \
+    hexo g
+
+# 预览
+docker run -it \
+    -p 4000:4000 \
+    -v $work_path/hexo:/Hexo \
+    hexo-docker \
+    hexo s --debug
